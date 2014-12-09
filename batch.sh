a9cdd8ed06efca584c56514a5ea943d38b020c41
Maxime Martineau <emixam@crocobox.org>
2014-12-10 00:46:44 +0100

    rubinius + learning

--- batch.sh
+++ batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# == 1 ]; then
 	for i in `seq 1 $1`; do
-		jruby client.rb 1jmomh47 arena 1;
+		rbx client.rb 1jmomh47 arena 1;
 	done
 else
 	echo "Usage: ./batch.sh NUMBER_OF_GAMES"
